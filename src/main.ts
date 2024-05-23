7be61439350cbedf0e318a5c114e1c4df08d59f0
Miguel B. <eomiguelbarros@gmail.com>
2024-05-23 08:56:50 -0300

    feat: implementing swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Summary notes for self-study')
+    .setDescription(
+      'API for an application to take notes on subjects studied in summary format',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();
